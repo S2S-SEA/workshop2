4dc01aa28ef8999a54f3275a09e648a3ca48fe71
tweeleng <41944804+tweeleng@users.noreply.github.com>
2018-08-10 11:46:50 +0800

    Update 2.process_ecmwf_threshold_weekly.py
    Final Format

--- code/model/ecmwf/prec/2.process_ecmwf_threshold_weekly.py
+++ code/model/ecmwf/prec/2.process_ecmwf_threshold_weekly.py
@@ -1,11 +1,12 @@
 '''
-1. Load the 'weekly rainfall XXth percentile climatology mask' and 'ECMWF_Total_Weekly'
+1. Load the 'daily rainfall XXth percentile climatology mask' and 'ECMWF_Data_Weekly'
 netCDF files.
-2. Calculate 'NDD' in the 1-week' from the 'ECMWF_Total_Weekly' dataset,
-by applying the 'Weekly Rainfall XXth percentile climatology mask and summing up the
+2. Calculate 'NDD' in 1week from the 'ECMWF_Data_Weekly' dataset,
+by applying the 'daily Rainfall XXth percentile climatology mask' and summing up the
 total number of dry days in each week.
-3. Output/Plot the 'NDD' in the 1-week for ECMWF climatology/total/anomaly.
+3. Output/Plot the 'NDD' in 1week for ECMWF model climatology/total/anomaly.
 '''
+
 import datetime
 import numpy as np
 import netCDF4
@@ -22,24 +23,26 @@ start_year        = 1998
 end_year          = 2014
 lead_times        = 4
 days              = 7
-members           = 11 #11 ensemble members
+members           = 11 #ensemble members
 threshold         = config.getint('Process','threshold')
 
 #Define ECMWF input data folder
-ec_input = '../../../../data/model/ecmwf/prec'
+ec_input  = '../../../../data/model/ecmwf/prec'
+#Define ECMWF output path and plot path
+ec_output = '../../../../data/model/ecmwf/prec/'
+plot_dir  = '../../../../plot/model/'
+
+#Choose to output and/or display data
+data_output = False
+plot_figure = False
 
 #--------------------------------------------------------------------------------------------------
-# 1. Load the two netCDF4 files: 'ECMWF_Total_Weekly' and 'Rainfall XXth percentile climatology mask'
+# 1. Load the two netCDF4 files: 'ECMWF_Data_Weekly' and 'Rainfall XXth percentile climatology mask'
 #--------------------------------------------------------------------------------------------------
-
-#Read 'ECMWF_Total_Weekly' data
-cur_ec_filename = 'ECMWF_' + calendar.month_abbr[target_month] + '_Total_Daily.nc'
+#Read 'ECMWF_Data_Weekly' data
+cur_ec_filename = 'ECMWF_' + calendar.month_abbr[target_month] + '_Data_Weekly.nc'
 cur_ec_path     = ec_input + '/' + cur_ec_filename
 ec_lat_weekly,ec_lon_weekly,ec_data = s2s_utility_prec.read_method(cur_ec_path,'ECMWF_Total')
-
-#print(np.shape(ec_lat_weekly))
-#print(np.shape(ec_lon_weekly))
-#print(np.shape(ec_data))
 
 #Read 'Rainfall XXth percentile climatology mask' data
 cur_ec_filename = 'ECMWF_' + calendar.month_abbr[target_month] + '_threshold' + str(threshold) + '_Climatology_Mask_Weekly.nc'
@@ -49,8 +52,6 @@ ec_lat_mask,ec_lon_mask,ec_climatology_mask = s2s_utility_prec.read_method(cur_e
 #---------------------------------------------------------------------------------------------
 # 2. This part is to calculate ECMWF (NDD) in a week [climatology/total/anomaly]
 #---------------------------------------------------------------------------------------------
-
-#Convert based on the ECMWF weekly 'Rainfall XXth percentile climatology mask' with minimum value of XXmm/day (XX=dynamic?)
 ec_climatology_mask[ec_climatology_mask < 1] = 1
 
 for i_year in range(0,end_year-start_year+1):
@@ -58,11 +59,8 @@ for i_year in range(0,end_year-start_year+1):
         for i_day in range(0,days):
             ec_data[:,:,i_year,i_day,i_member,:,:] = (ec_data[:,:,i_year,i_day,i_member,:,:] < ec_climatology_mask).astype(np.int_)
 
-#Sum up all 7 days in a week and compress the 'days' axis
-ec_total_ens     = np.sum(ec_data,axis=3) #use np.sum along axis3
-
-#Average over the 'ensemble member' axis
-ec_total_ens_avg = np.mean(ec_total_ens,axis=3)
+ec_total_ens     = np.sum(ec_data,axis=3) #Sum up all 7 days in a week and compress the 'days' axis. Use np.sum along axis3
+ec_total_ens_avg = np.mean(ec_total_ens,axis=3) #Average over the 'ensemble member' axis
 ec_climatology   = np.mean(ec_total_ens_avg,axis=2) #Obtain # of dry day in the week climatology by averaging over 'year' axis
 
 ec_anomaly       = np.empty([lead_times,len(week_initial_date),end_year-start_year+1,len(ec_lat_weekly),len(ec_lon_weekly)])
@@ -70,17 +68,8 @@ for i_year in range(0,end_year-start_year+1):
     ec_anomaly[:,:,i_year,:,:] = ec_total_ens_avg[:,:,i_year,:,:] - ec_climatology
 
 #--------------------------------------------------------------------------
-#2. This part is to output and display ECMWF NDD climatology/total/anomaly
+# 3. This part is to output and display ECMWF NDD climatology/total/anomaly
 #--------------------------------------------------------------------------
-
-#Define ECMWF output path and plot path
-ec_output = '../../../../data/model/ecmwf/prec/'
-plot_dir  = '../../../../plot/model/'
-
-#Choose to output or display data
-data_output = True
-plot_figure = True
-
 if data_output == True:
    #Output ECMWF climatology/total/anomaly
    ec_step = range(0,lead_times)
@@ -98,7 +87,7 @@ if data_output == True:
 
 if plot_figure == True:
    #Define target week and year
-   target_week = config.getint('Plot','target_week')    #week number starting from 0
+   target_week = config.getint('Plot','target_week')
    target_year = config.getint('Plot','target_year')
 
    #Define the domain for display
@@ -109,24 +98,24 @@ if plot_figure == True:
    grid_lat  = config.getint('Plot','grid_lat')
    grid_lon  = config.getint('Plot','grid_lon')
 
- #  ec_lat_weekly = ec_lat_weekly[::-1]    #reverse lat for matplotlib's plotting convention
-
    #Plot ECMWF NDD climatology/total/anomaly
    for i_step in range(0,lead_times):
        start_date = week_initial_date[target_week]
        end_date   = "%02d"%target_month + "%02d"%(int(start_date[-2:])+6)
 
-       data_range = [0,7]    #change data range for plotting accordingly
+       data_range = [0,days]
        title_str  = 'ECMWF NDD Weekly Climatology' + '\n' + start_date + '-' + end_date + ' (LT' + str(i_step+1) + ')'
        name_str   = plot_dir + 'ECMWF_' + start_date + '-' + end_date + '_' + 'LT' + str(i_step+1) + '_threshold' + str(threshold) + '_Climatology_Weekly.png'
        s2s_utility_prec.plot_processing(ec_climatology[i_step,target_week,:,:],ec_lat_weekly,ec_lon_weekly,lat_down,lat_up,lon_left,lon_right,grid_lat,grid_lon,data_range,title_str,name_str,'Climatology')
 
-       data_range = [0,7]
+       data_range = [0,days]
        title_str  = 'ECMWF NDD Weekly Total' + '\n' + str(target_year) + ' ' + start_date + '-' + end_date + ' (LT' + str(i_step+1) + ')'
        name_str   = plot_dir + 'ECMWF_' + str(target_year) + '_' + start_date + '-' + end_date + '_' + 'LT' + str(i_step+1) + '_threshold' + str(threshold) + '_Total_Weekly.png'
        s2s_utility_prec.plot_processing(ec_total_ens_avg[i_step,target_week,target_year-start_year,:,:],ec_lat_weekly,ec_lon_weekly,lat_down,lat_up,lon_left,lon_right,grid_lat,grid_lon,data_range,title_str,name_str,'Total')
 
-       data_range = [-7,7]
+       data_range = [-1*days,days]
        title_str  = 'ECMWF NDD Weekly Anomaly' + '\n' + str(target_year) + ' ' + start_date + '-' + end_date + ' (LT' + str(i_step+1) + ')'
        name_str   = plot_dir + 'ECMWF_' + str(target_year) + '_' + start_date + '-' + end_date + '_' + 'LT' + str(i_step+1) + '_threshold' + str(threshold) + '_Anomaly_Weekly.png'
        s2s_utility_prec.plot_processing(ec_anomaly[i_step,target_week,target_year-start_year,:,:],ec_lat_weekly,ec_lon_weekly,lat_down,lat_up,lon_left,lon_right,grid_lat,grid_lon,data_range,title_str,name_str,'Anomaly')
+        
+print('Finished!')
