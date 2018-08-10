b3964946364a66e07e558e900af652f7753076e3
tweeleng <41944804+tweeleng@users.noreply.github.com>
2018-08-10 11:54:42 +0800

    Update 2b.process_timeseries.py
    Final Format

--- code/model/ecmwf/prec/2b.process_timeseries.py
+++ code/model/ecmwf/prec/2b.process_timeseries.py
@@ -1,3 +1,8 @@
+'''
+Plot out the time series for dry days anomaly over the years for TRMMM and ECMWF model data for particular region
+All 4 lead times are included in the plot for the ECMWF model data
+'''
+
 import numpy
 import calendar
 from scipy import interpolate
@@ -14,13 +19,21 @@ end_year          = 2014
 lead_times        = 4
 threshold         = config.getint('Process','threshold')
 
+#Define the week for display, can be changed
+target_week = 0    # week number starting from 0
+#Define the domain for display, can be changed
+lat_down    = -20
+lat_up      = -10
+lon_left    = 110
+lon_right   = 120
+
 #---------------------------------------------------
 #This part is to prepare TRMM and ECMWF anomaly data
 #---------------------------------------------------
 
 #Define TRMM input path
 trmm_input    = '../../../../data/obs/prec'
-trmm_filename = 'TRMM_' + calendar.month_abbr[target_month] + '_threshold20_Anomaly_Weekly_EC_res.nc'
+trmm_filename = 'TRMM_' + calendar.month_abbr[target_month] + '_threshold' + str(threshold) + '_Anomaly_Weekly_EC_res.nc'
 cur_trmm_path = trmm_input + '/' + trmm_filename
 
 #Define ECMWF input path
@@ -35,16 +48,6 @@ ec_lat,ec_lon,ec_anomaly       = plot_timeseries.read_anomaly(cur_ec_path,'ECMWF
 #-----------------------------------------------------
 #This part is to display the time series for anomalies
 #-----------------------------------------------------
-
-#Define the week for display
-target_week = 0    # week number starting from 0, can be changed
-
-#Define the domain for display, can be changed
-lat_down  = -20
-lat_up    = -10
-lon_left  = 110
-lon_right = 120
-
 #Plot time series
 start_date = week_initial_date[target_week]
 print(start_date)
@@ -52,3 +55,5 @@ end_date   = "%02d"%target_month + "%02d"%(int(start_date[-2:])+6)
 title_str  = 'Number of Dry Days, Anomaly: Time Series' + '\n' + start_date + '-' + end_date
 name_str   = 'ECMWF_DryDays_Timeseries_' + start_date + '-' + end_date + '.png'
 plot_timeseries.plot_fig(trmm_anomaly,ec_anomaly,target_week,start_year,end_year,ec_lat,ec_lon,lat_down,lat_up,lon_left,lon_right,title_str,name_str)
+
+print('Finished!')
